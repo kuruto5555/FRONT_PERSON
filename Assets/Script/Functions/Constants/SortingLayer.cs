03e2ec3fcee40b289b0712f507019a73e56e1a39
Daijiro123 <jiro13970901@gmail.com>
2020-10-15 14:10:26 +0900

    定数クラスを追加しました。

---
+++ Assets/Script/Functions/Constants/SortingLayer.cs
@@ -0,0 +1,13 @@
+﻿namespace StealthPackMan.Constants
+{
+/// <summary>
+/// ソーティングレイヤー名を定数管理するクラス
+/// </summary>
+public static class SortingLayer
+{
+
+	public const string DEFAULT ="Default";
+
+}
+
+}
