0c4b310e6d115eff8f4c1ce65c1a9da01dfeaf95
snakegtrr35 <ksyougo0918@gmail.com>
2020-10-15 15:29:56 +0900

    FrontPerson.unity がなかったから、追加した

---
+++ Assets/Script/Kannno/Enemy/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrontPerson
+{
+    public class Enemy : MonoBehaviour
+    {
+        private Vector3 move_vec = new Vector3();
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
