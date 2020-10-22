5b9315173738ee592f9f9a91c8da5a28877519e6
Daijiro123 <jiro13970901@gmail.com>
2020-10-22 14:02:25 +0900

    シーンマネージャー追加

---
+++ Assets/Script/Kanamori/Manager/SceneManager/SceneManager.cs
@@ -0,0 +1,16 @@
+﻿using FrontPerson.common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace FrontPerson.Manager
+{
+    public class SceneManager : SingletonMonoBehaviour<SceneManager>
+    {
+        public void SceneChange(string scene_name, float interval_time)
+        {
+            FadeManager.Instance.LoadScene(scene_name, interval_time);
+        }
+    }
+}
