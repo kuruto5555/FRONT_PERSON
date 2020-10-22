f38d2144b56599818900af07ab39fe34f7bf2f91
Daijiro123 <jiro13970901@gmail.com>
2020-10-22 11:51:06 +0900

    名前空間を追加

--- Assets/Script/Kanamori/UI/VolumeSettingUI.cs
+++ Assets/Script/Kanamori/UI/VolumeSettingUI.cs
@@ -5,32 +5,35 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-public class VolumeSettingUI : MonoBehaviour
+namespace FrontPerson.UI
 {
-    [SerializeField]
-    private Slider volume_slider_ = null;
-    [SerializeField]
-    private Slider bgm_volume_slider_ = null;
-    [SerializeField]
-    private Slider se_volume_slider_ = null;
-
-    // Start is called before the first frame update
-    void Start()
+    public class VolumeSettingUI : MonoBehaviour
     {
-        if(!volume_slider_ ||
-            !bgm_volume_slider_ ||
-            !se_volume_slider_
-            )
+        [SerializeField]
+        private Slider volume_slider_ = null;
+        [SerializeField]
+        private Slider bgm_volume_slider_ = null;
+        [SerializeField]
+        private Slider se_volume_slider_ = null;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            Debug.LogError("音量設定のスライダーが設定されてません。");
+            if(!volume_slider_ ||
+                !bgm_volume_slider_ ||
+                !se_volume_slider_
+                )
+            {
+                Debug.LogError("音量設定のスライダーが設定されてません。");
+            }
+
+            volume_slider_.value = AudioManager.Instance.audio_volume_.Volume;
+            bgm_volume_slider_.value = AudioManager.Instance.audio_volume_.BGMVolume;
+            se_volume_slider_.value = AudioManager.Instance.audio_volume_.SEVolume;
+
+            volume_slider_.onValueChanged.AddListener((value) => { AudioManager.Instance.audio_volume_.Volume = value; });
+            bgm_volume_slider_.onValueChanged.AddListener((value) => { AudioManager.Instance.audio_volume_.BGMVolume = value; });
+            se_volume_slider_.onValueChanged.AddListener((value) => { AudioManager.Instance.audio_volume_.SEVolume = value; });
         }
-
-        volume_slider_.value = AudioManager.Instance.audio_volume_.Volume;
-        bgm_volume_slider_.value = AudioManager.Instance.audio_volume_.BGMVolume;
-        se_volume_slider_.value = AudioManager.Instance.audio_volume_.SEVolume;
-
-        volume_slider_.onValueChanged.AddListener((value) => { AudioManager.Instance.audio_volume_.Volume = value; });
-        bgm_volume_slider_.onValueChanged.AddListener((value) => { AudioManager.Instance.audio_volume_.BGMVolume = value; });
-        se_volume_slider_.onValueChanged.AddListener((value) => { AudioManager.Instance.audio_volume_.SEVolume = value; });
     }
 }
