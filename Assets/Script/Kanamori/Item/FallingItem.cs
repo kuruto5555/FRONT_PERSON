1d0cbeb899ea8b1f7e34debb395ad16aad8bb8e4
Daijiro123 <jiro13970901@gmail.com>
2020-11-13 16:16:57 +0900

    命名を修正 落ちているアイテムに挙動を持たせました。 マネージャーに自身を管理させます。

---
+++ Assets/Script/Kanamori/Item/FallingItem.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FrontPerson.Manager;
+
+namespace FrontPerson.Item
+{
+    /// <summary>
+    /// フィールド上に落ちているアイテムの基底クラス
+    /// </summary>
+    public abstract class FallingItem : MonoBehaviour
+    {
+        /// <summary>
+        /// 回転速度
+        /// </summary>
+        private readonly float ROTATIONSPEED = 1f;
+        /// <summary>
+        /// ふわふわ浮く移動量
+        /// </summary>
+        private readonly float FLUFFY_MOOVEMENT = 0.5f;
+        /// <summary>
+        /// ふわふわの浮き沈みの速度
+        /// </summary>
+        private readonly float FLUFFY_SPEED = 2f;
+
+        private FallingItemManager falling_item_manager_;
+
+        protected Transform transform_;
+
+        private void Start()
+        {
+            // アイテムマネージャーにアイテムを管理させる。
+            falling_item_manager_ = FindObjectOfType<FallingItemManager>();
+            falling_item_manager_.AddItem(this);
+
+            transform_ = transform;
+
+            OnStart();
+        }
+
+        private void Update()
+        {
+            // フィールド上に落ちているアイテムの挙動
+            MovementItem();
+
+            OnUpdate();
+        }
+
+        /// <summary>
+        /// アイテムを拾った
+        /// </summary>
+        public void TakenItem(PlayerInventory inventory)
+        {
+            OnTakenItem(inventory);
+
+            // マネージャーから消去
+            falling_item_manager_.RemoveItem(this);
+        }
+
+        /// <summary>
+        /// 落ちているアイテムを削除する
+        /// </summary>
+        public void DestroyItem()
+        {
+            OnDestroyItem();
+
+            GameObject.Destroy(this.gameObject);
+        }
+
+        /// <summary>
+        /// 出現中のアイテムに挙動をもたせる。
+        /// </summary>
+        public void MovementItem()
+        {
+            // 回転
+            transform_.Rotate(new Vector3(0f, ROTATIONSPEED, 0f));
+
+            // ふわふわ浮かせる
+            transform_.position = new Vector3(transform_.position.x, Mathf.PingPong(Time.time / FLUFFY_SPEED, FLUFFY_MOOVEMENT), transform_.position.z);
+        }
+
+        /* ---- 抽象関数 ----*/
+        /// <summary>
+        /// アイテムを拾った
+        /// </summary>
+        protected abstract void OnTakenItem(PlayerInventory inventory);
+        /// <summary>
+        /// 落ちているアイテムを削除する
+        /// </summary>
+        protected abstract void OnDestroyItem();
+        /// <summary>
+        /// 開始
+        /// </summary>
+        protected abstract void OnStart();
+        /// <summary>
+        /// 更新
+        /// </summary>
+        protected abstract void OnUpdate();
+    }
+}
