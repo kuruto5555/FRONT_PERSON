2379a7400988fac7f0a6ce3475a5f54dbaec3b3a
snakegtrr35 <ksyougo0918@gmail.com>
2020-11-13 16:36:10 +0900

    ファルダ移動

---
+++ Assets/Script/Kannno/Object/BillBoard.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrontPerson
+{
+    public class BillBoard : MonoBehaviour
+    {
+        /// <summary>
+        /// カメラ
+        /// </summary>
+        private Camera targetCamera = null;
+
+        /// <summary>
+        /// 裏表反対になることを直す為の変数
+        /// </summary>
+        private Quaternion rotate = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+
+        /// <summary>
+        /// 2フレーム毎に更新させる為の変数
+        /// </summary>
+        bool enable = true;
+
+#if UNITY_EDITOR
+        void Start()
+        {
+            if(null == targetCamera)
+            {
+                targetCamera = FindObjectOfType<Camera>();
+            }
+        }
+#endif
+
+        void Update()
+        {
+            if (enable)
+            {
+                Vector3 target = targetCamera.transform.position;
+                target.y = transform.position.y;
+                transform.LookAt(target);
+
+                transform.rotation *= rotate;
+
+                enable = !enable;
+            }
+            else
+            {
+                enable = !enable;
+            }
+        }
+    }
+}
