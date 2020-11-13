443bedab9ae9169bacbebcc74dbeda8dc687031b
yuyaoyukiデスクトップPC <h.kuruto.0505@gmail.com>
2020-11-13 20:21:18 +0900

    ビタミン名前変更

--- Assets/Script/ooyuki/Vitamin/VitaminType.cs
+++ Assets/Script/ooyuki/Vitamin/VitaminType.cs
@@ -4,35 +4,29 @@
 
 namespace FrontPerson.Constants
 {
-    public enum VITAMIN_TYPE
+    public enum NUTRIENTS_TYPE
     {
-        VITAMIN_A,
-        VITAMIN_B,
-        VITAMIN_C,
-        VITAMIN_D,
-        VITAMIN_ALL,
+        NUTRIENTS_A,
+        NUTRIENTS_B,
+        NUTRIENTS_ALL,
 
         COUNT
     }
 
     // ビタミンから栄養素の変わったので変更
     // Vitamin -> Nutrientsにする
-    public static class Vitamin
+    public static class Nutrients
 	{
         public static List<string> Type = new List<string>() 
         {
-            "VitaminA",
-            "VitaminB",
-            "VitaminC",
-            "VitaminD",
-            "VitaminALL"
+            "NutrientsA",
+            "NutrientsB",
+            "NutrientsALL"
         };
 
-        //public const string VITAMIN_A = "VitaminA";
-        //public const string VITAMIN_B = "VitaminB";
-        //public const string VITAMIN_C = "VitaminC";
-        //public const string VITAMIN_D = "VitaminD";
-        //public const string VITAMIN_ALL = "VitaminALL";
+        //public const string NUTRIENTS_A   = "NutrientsA";
+        //public const string NUTRIENTS_A   = "NutrientsB";
+        //public const string NUTRIENTS_ALL = "NutrientsALL";
     }
 
 }
