738c3f33c6012ea11e7a20ab0c0bdcba8a3c06dd
snakegtrr35 <ksyougo0918@gmail.com>
2020-11-13 16:36:58 +0900

    スポナーの追加とMovePointの修正

---
+++ Assets/Script/Kannno/Enemy/AI/EnemyState_Attack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using FrontPerson.Character;
+
+namespace FrontPerson.Enemy.AI
+{
+    public class EnemyState_Attack : EnemyState_AI
+    {
+        private SearchArea searchArea = null;
+
+        private Player Player = null;
+
+        protected override void OnStart()
+        {
+            searchArea = GetComponentInChildren<SearchArea>();
+
+            Player = FindObjectOfType<Player>();
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+
+        protected override void OnChangeState_OrdinaryPeople()
+        {
+        }
+
+        protected override void OnChangeState_OldBattleaxe()
+        {
+            if (!searchArea.IsFound)
+            {
+                ChangeState<EnemyState_Search>();
+
+                OldBattleaxe enemy = Owner as OldBattleaxe;
+
+                enemy.isHit = false;
+            }
+        }
+
+        protected override void OnChangeState_Yakuza()
+        {
+        }
+    }
+}
