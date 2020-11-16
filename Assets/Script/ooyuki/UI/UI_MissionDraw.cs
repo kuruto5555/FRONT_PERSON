268eef7aad425efc1d7b7e658cf57ab6542e09c8
yuyaoyukiデスクトップPC <h.kuruto.0505@gmail.com>
2020-11-16 21:54:18 +0900

    ミッション表示用Script作成

---
+++ Assets/Script/ooyuki/UI/UI_MissionDraw.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FrontPerson.UI
+{
+    public class UI_MissionDraw : MonoBehaviour
+    {
+        [Header("バウンティーマネージャー")]
+        [SerializeField]
+        BountyManager bountyManager_ = null;
+
+        /// <summary>
+        /// ミッション名
+        /// </summary>
+        [Header("ミッション1")]
+        [SerializeField]
+        Text mission1NameText_ = null;
+        /// <summary>
+        /// 進捗表示用テキスト
+        /// </summary>
+        [SerializeField]
+        Text mission1ProgressText_ = null;
+        /// <summary>
+        /// 残り時間タイマー
+        /// </summary>
+        [SerializeField]
+        Image mission1Timer_ = null;
+
+
+        [Header("ミッション2")]
+        [SerializeField]
+        Text mission2NameText_ = null;
+        [SerializeField]
+        Text mission2ProgressText_ = null;
+        [SerializeField]
+        Image mission2Timer_ = null;
+
+
+        [Header("ミッション3")]
+        [SerializeField]
+        Text mission3NameText_ = null;
+        [SerializeField]
+        Text mission3ProgressText_ = null;
+        [SerializeField]
+        Image mission3Timer_ = null;
+
+
+        void Start()
+        {
+            SetText();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            SetText();
+        }
+
+        void SetText()
+        {
+            //mission1NameText_.text = bountyManager_.;
+            //mission1ProgressText_.text = bountyManager_.;
+            //mission1Timer_. = bountyManager_.;
+
+            //mission2NameText_.text = bountyManager_.;
+            //mission2ProgressText_.text = bountyManager_.;
+            //mission2Timer_. = bountyManager_.;
+
+            //mission3NameText_.text = bountyManager_.;
+            //mission3ProgressText_.text = bountyManager_.;
+            //mission3Timer_. = bountyManager_.;
+        }
+    }
+}
