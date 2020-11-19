8970ae28feda4dcb178a2924fd94896da9ead2f4
arai <bythx790@gmail.com>
2020-11-19 11:06:24 +0900

    バウンティ一通り仮実装

---
+++ Assets/Script/Arai/Bounty/BountyCombo.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrontPerson.Bounty
+{
+    public class BountyCombo : Bounty
+    {
+        [Header("コンボ数")]
+        [SerializeField,Range(1, 100)] int ComboMax = 10;
+
+        //private bool _isStart = false;
+
+        public int GetComboMax { get { return ComboMax; } }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            base.Start();
+
+            _progressString = _Bmanager.GetNowCombo().ToString();
+
+            //_isStart = false;
+            //_text.text = ComboMax + MissionName;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            base.Update();
+
+            int nowCombo = _Bmanager.GetNowCombo();
+
+            _progressString = nowCombo.ToString();
+
+            //if (!_isStart)
+            //{
+            //    if (nowCombo > 1) _isStart = true;
+            //}
+            //else //１度コンボをミスしたら終了
+            //{
+            //    if (nowCombo == 0) _isFinish = true;
+            //}
+
+            if(nowCombo > ComboMax)
+            {
+                MissionClear();
+            }
+        }
+    }
+}
---
+++ Assets/Script/Arai/Bounty/BountyNoMissCombo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrontPerson.Bounty
+{
+    public class BountyNoMissCombo : Bounty
+    {
+        /// <summary>
+        /// コンボが始まっているか
+        /// </summary>
+        private bool _isComboStart = false;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            base.Start();
+
+            _isComboStart = false;
+            //_text.text = LimitTime + MissionName;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            base.Update();
+            
+            int nowCombo = _Bmanager.GetNowCombo();
+
+            if (!_isComboStart)
+            {
+                if (nowCombo > 1) _isComboStart = true;
+                //if(GetLimitTime < GetNowTime) _isClear = _isFinish = true;
+                _nowTime = LimitTime; //コンボが始まるまでカウントダウンを止めておく
+            }
+            else //コンボがスタートしてる間
+            {
+
+                if (GetLimitTime < GetNowTime) MissionClear();
+
+                //コンボが初期化されたら失敗
+                if (nowCombo == 0) MissionFailed(); ;
+                
+            }
+        }
+    }
+}
