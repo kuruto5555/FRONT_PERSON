051fc062c7ac0793820dc3ec39028a4ae9a849c2
arai <bythx790@gmail.com>
2020-11-26 20:58:09 +0900

    WeaponListバグ修正

---
+++ Assets/Script/Arai/Weapon/Bullet/Missile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 追尾処理
+    /// </summary>
+    private void Homing()
+    {
+       // float step = Time.deltaTime * HomingSpeed_;
+       //
+       // transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, step);
+       //
+       // var rot = transform.rotation;
+       //
+       // transform.LookAt(_player.transform);
+    }
+}
