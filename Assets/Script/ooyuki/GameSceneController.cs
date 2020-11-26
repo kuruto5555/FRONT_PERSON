1c435f18a7d31b47ec7aacf6cadbe893cd25c5b4
yuyaoyukiデスクトップPC <h.kuruto.0505@gmail.com>
2020-11-26 19:39:25 +0900

    フォント追加した。 みんなに共有するために一度コミット。

---
+++ Assets/Script/ooyuki/GameSceneController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrontPerson.Manager
+{
+    public class GameSceneController : MonoBehaviour
+    {
+        enum GAME_SCENE_STATE
+        {
+            TUTORIAL = 0,
+            PLAY,
+            TIME_UP,
+            TRANSITION
+        }
+        GAME_SCENE_STATE state_ = GAME_SCENE_STATE.TUTORIAL;
+
+
+
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            state_ = GAME_SCENE_STATE.TUTORIAL;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            switch (state_)
+            {
+                case GAME_SCENE_STATE.TUTORIAL:
+                    TutorialUpdate();
+                    break;
+
+                case GAME_SCENE_STATE.PLAY:
+                    PlayUpdate();
+                    break;
+
+                case GAME_SCENE_STATE.TIME_UP:
+                    TimeUpUpdate();
+                    break;
+
+                case GAME_SCENE_STATE.TRANSITION:
+                    TransitionUpdate();
+                    break;
+            }
+        }
+
+        void TutorialUpdate()
+        {
+
+        }
+
+        void PlayUpdate()
+        {
+
+        }
+
+        void TimeUpUpdate()
+        {
+
+        }
+
+        void TransitionUpdate()
+        {
+
+        }
+    }
+}
