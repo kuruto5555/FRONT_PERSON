a7227ae12655e4f200c5315c67149fd47c3e815c
snakegtrr35 <ksyougo0918@gmail.com>
2020-11-27 15:22:06 +0900

    セーブする用のcsファイルの追加

---
+++ Assets/Script/Kannno/Datas/SoundVolumeData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrontPerson.Data
+{
+    [System.Serializable]
+    public class SoundVolumeData
+    {
+        public SoundVolumeData()
+        {
+            volume_ = 1f;
+            bgm_volume_ = 1f;
+            se_volume_ = 1f;
+        }
+        public float volume_;
+        public float bgm_volume_;
+        public float se_volume_;
+    }
+}
