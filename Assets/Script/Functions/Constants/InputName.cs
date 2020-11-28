c366d0cba5fdd44c2739a4e939044925bb2e62f5
Daijiro123 <jiro13970901@gmail.com>
2020-11-28 14:31:43 +0900

    Input情報変更

--- Assets/Script/Functions/Constants/InputName.cs
+++ Assets/Script/Functions/Constants/InputName.cs
@@ -7,13 +7,18 @@ public static class InputName
 {
 
 	public const string CANCEL                     ="Cancel";
+	public const string DASH                       ="Dash";
 	public const string FIRE1                      ="Fire1";
 	public const string FIRE2                      ="Fire2";
-	public const string FIRE3                      ="Fire3";
 	public const string HORIZONTAL                 ="Horizontal";
+	public const string HORIZONTAL2                ="Horizontal2";
 	public const string JUMP                       ="Jump";
+	public const string PAUSE                      ="Pause";
+	public const string RELOAD                     ="Reload";
+	public const string SCAN                       ="Scan";
 	public const string SUBMIT                     ="Submit";
 	public const string VERTICAL                   ="Vertical";
+	public const string VERTICAL2                  ="Vertical2";
 
 }
 
