3781b5275c6ed1a917c949768189c12c69125c32
daikiisozaki <daikiisozaki@gmail.com>
2020-12-03 19:01:55 +0900

    Sound再生のスクリプトだけ作った

---
+++ Assets/Script/zaki/PlaySound.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace FrontPerson
+{
+    public class PlaySound : MonoBehaviour
+    {
+        //オーディオソース
+        private AudioSource audio = null;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            audio = GetComponent<AudioSource>();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+        public void SoundPlay()
+        {
+            //
+            if (audio == null)
+            {
+                audio = gameObject.AddComponent<AudioSource>();
+            }
+
+        }
+    }
+}
