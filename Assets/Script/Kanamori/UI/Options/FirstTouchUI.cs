3751ace24d64e75a231433259e5da4ce1a8a0c93
Daijiro123 <jiro13970901@gmail.com>
2020-12-10 15:34:07 +0900

    ゲームパッドでメニューを操作すると選択物が光らないので修正しました。

--- Assets/Script/Kanamori/UI/Options/FirstTouchUI.cs
+++ Assets/Script/Kanamori/UI/Options/FirstTouchUI.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace FrontPerson.UI
 {
@@ -9,15 +10,31 @@ namespace FrontPerson.UI
     {
         [SerializeField]
         private GameObject first_touch_object_ = null;
+        private EventSystem event_system_;
 
         // Start is called before the first frame update
         void Start()
         {
-            EventSystem.current.SetSelectedGameObject(first_touch_object_);
+            event_system_ = EventSystem.current;
+            event_system_.SetSelectedGameObject(first_touch_object_);
+            BaseEventData baseEventData = new BaseEventData(event_system_);
+            baseEventData.selectedObject = event_system_.currentSelectedGameObject;
+            first_touch_object_.GetComponent<Selectable>().OnSelect(baseEventData);
+            baseEventData.Reset();
         }
+
         private void OnEnable()
         {
-            EventSystem.current.SetSelectedGameObject(first_touch_object_);
+            if (event_system_ == null)
+            {
+                event_system_ = EventSystem.current;
+            }
+
+            event_system_.SetSelectedGameObject(first_touch_object_);
+            BaseEventData baseEventData = new BaseEventData(event_system_);
+            baseEventData.selectedObject = event_system_.currentSelectedGameObject;
+            first_touch_object_.GetComponent<Selectable>().OnSelect(baseEventData);
+            baseEventData.Reset();
         }
     }
 }
