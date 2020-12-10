a47d871d683a5bf2c38f34daa66e957bcc5d6db1
Daijiro123 <jiro13970901@gmail.com>
2020-12-10 14:41:53 +0900

    AIM感度調整できるようにしました。

---
+++ Assets/Script/Kanamori/UI/Options/Sensitivity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FrontPerson.UI
+{
+    public class Sensitivity : MonoBehaviour
+    {
+        [SerializeField]
+        private Slider sensi_slider_;
+        private Character.Player player_;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // ごめんなさい。
+            player_ = GameObject.FindGameObjectWithTag(Constants.TagName.PLAYER).GetComponent<Character.Player>();
+
+            if (player_)
+            {
+                sensi_slider_.onValueChanged.AddListener((value) => player_.SetViewRotateSpeed((int)value));
+            }
+        }
+    }
+}
