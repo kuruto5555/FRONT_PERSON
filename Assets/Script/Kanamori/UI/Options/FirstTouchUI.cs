5ba5378ed8196fdbc2b732a2c44219b281bef82d
Daijiro123 <jiro13970901@gmail.com>
2020-12-10 14:06:10 +0900

    オプションシーン用に各種スクリプト追加

---
+++ Assets/Script/Kanamori/UI/Options/FirstTouchUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace FrontPerson.UI
+{
+    public class FirstTouchUI : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject first_touch_object_ = null;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            EventSystem.current.SetSelectedGameObject(first_touch_object_);
+        }
+        private void OnEnable()
+        {
+            EventSystem.current.SetSelectedGameObject(first_touch_object_);
+        }
+    }
+}
