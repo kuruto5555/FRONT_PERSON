8469d2df67e33f88dad6e6a313ad023cd9b9900c
Daijiro123 <jiro13970901@gmail.com>
2020-12-10 15:35:04 +0900

    センシを反映させるようにしました

--- Assets/Script/Kanamori/UI/Options/Sensitivity.cs
+++ Assets/Script/Kanamori/UI/Options/Sensitivity.cs
@@ -8,14 +8,19 @@ namespace FrontPerson.UI
     public class Sensitivity : MonoBehaviour
     {
         [SerializeField]
-        private Slider sensi_slider_;
+        private Slider sensi_slider_ = null;
         private Character.Player player_;
 
         // Start is called before the first frame update
         void Start()
         {
             // ごめんなさい。
-            player_ = GameObject.FindGameObjectWithTag(Constants.TagName.PLAYER).GetComponent<Character.Player>();
+            var obj = GameObject.FindGameObjectWithTag(Constants.TagName.PLAYER);
+
+            if (obj)
+            {
+                player_ = obj.GetComponent<Character.Player>();
+            }
 
             if (player_)
             {
