7f73334043d76c4579bfbe98e6259798960836ca
snakegtrr35 <ksyougo0918@gmail.com>
2020-12-15 01:12:20 +0900

    アイテム取得のUIとアニメーションとスクリプト(仮)

---
+++ Assets/Script/Kannno/UI/PickupItem/PickupItemUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrontPerson.UI
+{
+    public class PickupItemUI : MonoBehaviour
+    {
+        static readonly int PICKUP_ITEM = Animator.StringToHash("PickupItem");
+
+        private Animator animator = null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private bool flag = true;
+
+        private void OnEnable()
+        {
+            if(flag)
+            {
+                animator?.Play(PICKUP_ITEM);
+
+                flag = false;
+            }
+        }
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+
+            if (flag)
+            {
+                animator?.Play(PICKUP_ITEM);
+
+                flag = false;
+            }
+        }
+
+        private void Enable()
+        {
+            flag = true;
+        }
+    }
+}
