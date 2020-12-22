3d6659505b92a069d24649821b5c00369e5e2ab7
yuyaoyukiデスクトップPC <h.kuruto.0505@gmail.com>
2020-12-22 20:20:02 +0900

    ランキングシーンを追加

--- Assets/Script/Functions/Constants/SceneName.cs
+++ Assets/Script/Functions/Constants/SceneName.cs
@@ -6,10 +6,11 @@
 public static class SceneName
 {
 
-	public const string GAME_SCENE   ="GameScene";
-	public const string KANAMORI     ="Kanamori";
-	public const string RESULT_SCENE ="ResultScene";
-	public const string TITLE_SCENE  ="TitleScene";
+	public const string GAME_SCENE    ="GameScene";
+	public const string KANAMORI      ="Kanamori";
+	public const string RANKING_SCENE ="RankingScene";
+	public const string RESULT_SCENE  ="ResultScene";
+	public const string TITLE_SCENE   ="TitleScene";
 
 }
 
