dd40f0aec4741690bc639755a8e96af52c588e1a
yuyaoyukiデスクトップPC <h.kuruto.0505@gmail.com>
2020-12-22 12:56:39 +0900

    カーソルマネージャー作成。

---
+++ Assets/Script/ooyuki/Manager/CursorManager.cs
@@ -0,0 +1,23 @@
+﻿using FrontPerson.common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrontPerson.Manager
+{
+    public class CursorManager : SingletonMonoBehaviour<CursorManager>
+    {
+        public void CursorLock()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+
+        public void CurcorUnlock()
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
